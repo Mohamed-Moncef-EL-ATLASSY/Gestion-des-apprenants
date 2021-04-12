6b913066e88fb1b05b635833cb2433df89d16455
Mohamed Moncef EL ATLASSY <mohamed.moncef.elatlassy@gmail.com>
2021-04-12 01:30:12 +0000

    les petites retouches

--- Connect.php
+++ Connect.php
@@ -4,12 +4,5 @@ $user="root";
 $password="";
 $db="brief4";
 $con=new mysqli($host,$user,$password,$db);
-// if($con)
-// {
-//     echo"connected";
-// }
-// else
-// {
-//     echo"erreur de connection";
-// }
+
 ?>
